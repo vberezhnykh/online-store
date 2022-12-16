d8eea46903d63d7a5c863baf79c13b15850f1acc
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-12-16 15:46:02 +0300

    feat: add stock filter

--- src/components/filters/filters.ts
+++ src/components/filters/filters.ts
@@ -247,20 +247,49 @@ class Price extends Element {
   }
 }
 
+class Stock extends Element {
+  _dualSlider: DualSlider;
+  _minStock: number;
+  _maxStock: number;
+  constructor() {
+    super("div", "stock");
+    this._dualSlider = new DualSlider();
+    this._minStock = Math.min(...PRODUCTS.map((product) => product.stock));
+    this._maxStock = Math.max(...PRODUCTS.map((product) => product.stock));
+  }
+  createHeading() {
+    const heading = document.createElement("h3");
+    heading.classList.add("stock__heading");
+    heading.textContent = "Stock";
+    return heading;
+  }
+
+  draw() {
+    this._element.appendChild(this.createHeading());
+    this._element.appendChild(
+      this._dualSlider.drawDualSlider(this._minStock, this._maxStock)
+    );
+    return this._element;
+  }
+}
+
 class Filters extends Element {
   _categoryFilter: CategoryList;
   _brandsFilter: BrandList;
   _price: Price;
+  _stock: Stock;
   constructor() {
     super("aside", "filters");
     this._categoryFilter = new CategoryList();
     this._brandsFilter = new BrandList();
     this._price = new Price();
+    this._stock = new Stock();
   }
   draw() {
     this._element.appendChild(this._categoryFilter.draw());
     this._element.appendChild(this._brandsFilter.draw());
     this._element.appendChild(this._price.draw());
+    this._element.appendChild(this._stock.draw());
     document.querySelector(".page__container")?.prepend(this._element);
     // this._element.onclick = (event) => this.applyFilter(event);
   }
