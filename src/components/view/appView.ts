68fbe2caf70b97a7f42943bcc97b39555ee0c5fc
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-12-25 18:58:53 +0300

    feat: add basic layout of modal window

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -2,23 +2,27 @@ import Header from "./header/header";
 import Footer from "./footer/footer";
 import Main from "./pages/main/mainPage";
 import Details from "./pages/details/detailsPage";
+import Modal from "./pages/modal/modal";
 
 class AppView {
   header: Header;
   footer: Footer;
   //page: Main;
-  page: Details;
+  // page: Details;
+  modal: Modal;
 
   constructor() {
     this.header = new Header();
     this.footer = new Footer();
-    this.page = new Details();
+    // this.page = new Details();
+    this.modal = new Modal();
   }
 
   drawPage() {
     this.header.draw();
     this.footer.draw();
-    this.page.draw();
+    // this.page.draw();
+    this.modal.draw();
   }
 }
 
