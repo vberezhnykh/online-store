ed595f7b84f8a87d4f960c7db58ce2cd7279910f
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-12-26 20:56:43 +0300

    feat: add overlay

--- src/components/view/pages/modal/modal.ts
+++ src/components/view/pages/modal/modal.ts
@@ -10,10 +10,23 @@ class Modal {
     this._cardDetails = new CardDetails();
   }
   draw() {
+    const overlay = createCustomElement({
+      selector: "div",
+      class: "modal-overlay",
+    });
+    document.body.prepend(overlay);
     const modal = createCustomElement({ selector: "div", class: "modal" });
     modal.append(this._personalDetails.createPersonalDetails());
     modal.append(this._cardDetails.createCardDetails());
-    document.body.prepend(modal);
+    overlay.append(modal);
+    overlay.onclick = (event) => this.closeModal(event);
+  }
+  closeModal(event: Event) {
+    if (
+      event.target instanceof HTMLDivElement &&
+      event.target.classList.contains("modal-overlay")
+    )
+      event.target.remove();
   }
 }
 
