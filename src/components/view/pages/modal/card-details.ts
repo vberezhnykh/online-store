a587ad8ab1c819f09715b0e844ba7eabee8a9181
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-12-27 22:51:23 +0300

    feat: add card number validation

--- src/components/view/pages/modal/card-details.ts
+++ src/components/view/pages/modal/card-details.ts
@@ -2,6 +2,7 @@ import { createCustomElement } from "../../../../assets/misc/func";
 import creditCardImgSrc from "../../../../assets/images/modal/credit-card.png";
 
 export class CardDetails {
+  _cardNumberValidation = false;
   createCardDetails() {
     const container = createCustomElement({
       selector: "div",
@@ -30,6 +31,8 @@ export class CardDetails {
       createCustomElement({ selector: "input", class: "card-number__input" })
     );
     cardNumberInput.placeholder = "Card number";
+    cardNumberInput.type = "number";
+    cardNumberInput.onkeyup = () => this.validateCardNumber(cardNumberInput);
     cardNumberContainer.append(cardNumberInput);
     cardData.append(cardNumberContainer);
 
@@ -61,6 +64,32 @@ export class CardDetails {
     cardData.append(otherCardData);
 
     container.append(cardData);
+
+    const ERROR_CLASSES = ["card-number", "valid", "cvv"];
+    for (let i = 0; i < 3; i++) {
+      const error = createCustomElement({
+        selector: "div",
+        class: `${ERROR_CLASSES[i]}__error`,
+      });
+      error.textContent = `${ERROR_CLASSES[i]} - error`;
+      container.appendChild(error);
+    }
+
     return container;
+  }
+
+  toggleErrorMessage(className: string, isValid: boolean) {
+    const errorElement = document.querySelector(`.${className}__error`);
+    if (errorElement)
+      if (isValid) errorElement.classList.remove(`${className}__error--active`);
+      else errorElement.classList.add(`${className}__error--active`);
+  }
+
+  validateCardNumber(input: HTMLInputElement) {
+    this._cardNumberValidation = false;
+    if (input.value.length > 16) input.value = input.value.slice(0, 16);
+    if (input.value.length === 16) this._cardNumberValidation = true;
+    console.log(this._cardNumberValidation);
+    return this.toggleErrorMessage("card-number", this._cardNumberValidation);
   }
 }
