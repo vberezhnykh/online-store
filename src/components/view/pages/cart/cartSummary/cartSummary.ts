a188b07f5539201b3c9db53b1c80545d546592bb
l1ghtm00n <lizardfrogdamn@gmail.com>
2022-12-28 14:53:30 +0300

    feat: complete the design of summary

--- src/components/view/pages/cart/cartSummary/cartSummary.ts
+++ src/components/view/pages/cart/cartSummary/cartSummary.ts
@@ -15,25 +15,38 @@ class CartSummary {
 
   createPromo() {
     const promo = <HTMLDivElement>createCustomElement({selector : 'div', class : 'summary__promo'});
-    const code = <HTMLInputElement>createCustomElement({selector : 'input', class : 'summary__promo-code'});
-    const test = <HTMLSpanElement>createCustomElement({selector : 'span', class : 'summary__promo-test'});
-
+    const code = <HTMLInputElement>createCustomElement({selector : 'input', class : 'promo__code'});
+    const test = <HTMLSpanElement>createCustomElement({selector : 'span', class : 'promo__test'});
+    code.type = 'search';
+    code.value = 'Enter promo code here';
     test.textContent = `${types.CartText.summaryPromoTest}${types.CartText.promoTestCodes}`;
     promo.appendChild(code);
     promo.appendChild(test);
     return promo;
   }
 
-  createTotal(data: Pick<types.IProductInfo, 'stock' | 'price'>) {
+  createTotal(data: Pick<types.IProductInfo, 'price'>) {
+    const total = <HTMLDivElement>createCustomElement({selector : 'div', class : 'summary__total'});
+    const totalAmount = <HTMLParagraphElement>createCustomElement({selector : 'p', class : 'total__text'});
+    const totalPrice = <HTMLParagraphElement>createCustomElement({selector : 'p', class : 'total__text'});
+    const totalAmountValue = <HTMLSpanElement>createCustomElement({selector : 'span', class : 'total__value total-amount'});
+    const totalPriceValue = <HTMLSpanElement>createCustomElement({selector : 'span', class : 'total__value total-price'});
+    totalAmount.textContent = `${types.CartText.summaryProduct}`;
+    totalAmountValue.textContent = ` ${1}`;
+    totalAmount.appendChild(totalAmountValue);
+    totalPrice.textContent = `${types.CartText.summaryTotal}`;
+    totalPriceValue.textContent = ` $${data.price}`;
+    totalPrice.appendChild(totalPriceValue);
+    total.appendChild(totalAmount);
+    total.appendChild(totalPrice);
+    return total;
+  }
+
+  createContent(data: Pick<types.IProductInfo, 'price'>) {
     const quantity = <HTMLDivElement>createCustomElement({selector : 'div', class : 'summary__content'});
-    const amount = <HTMLSpanElement>createCustomElement({selector : 'span', class : 'summary__total-amount'});
-    const total = <HTMLSpanElement>createCustomElement({selector : 'span', class : 'summary__total-price'});
-    amount.textContent = `${types.CartText.summaryProduct} ${1}`;
-    total.textContent = `${types.CartText.summaryTotal} $${data.price}`;
     const btnBuyNow = <HTMLButtonElement>createCustomElement({selector : 'button', class : 'summary__button'});
     btnBuyNow.textContent = 'Buy now'.toUpperCase();
-    quantity.appendChild(amount);
-    quantity.appendChild(total);
+    quantity.appendChild(this.createTotal(data));
     quantity.appendChild(this.createPromo());
     quantity.appendChild(btnBuyNow);
     return quantity;
@@ -42,7 +55,7 @@ class CartSummary {
   createSummary(data: Omit<types.IProductInfo, 'images'>) {
     const summary = <HTMLDivElement>createCustomElement({selector : 'div', class : 'cart__summary cart-col'});
     summary.appendChild(this.createHeading());
-    summary.appendChild(this.createTotal(data));
+    summary.appendChild(this.createContent(data));
     return summary;
   }
 
